4895a29cb68fcda738ceeeaab8ffa9d3392bf6fa
Daniel Nalesnik <nalesnik@Daniels-MacBook-Pro.local>
2018-01-18 21:19:02 +0800

    add mailgun

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,4 +22,26 @@ Myflix::Application.configure do
   # use gem 'letter_opener' to preview mails sent
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+
+
+
+
+    ActionMailer::Base.smtp_settings = {
+      :port           => ENV['MAILGUN_SMTP_PORT'],
+      :address        => ENV['MAILGUN_SMTP_SERVER'],
+      :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+      :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+      :domain         => 'localhost:3000',
+      :authentication => :plain,
+    }
+    ActionMailer::Base.delivery_method = :smtp
+
+
+
+
+
+
+
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,18 @@ Myflix::Application.configure do
   config.action_mailer.default_url_options = { host: 'https://immense-dusk-34126.herokuapp.com/' }
 
 
-  # test if below will suppress mail delivery errors
-  config.action_mailer.raise_delivery_errors = false
+
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
+
+  # enabling below will suppress email errors
+  # config.action_mailer.raise_delivery_errors = false
 end
